40e08f78041711249a2adffa41ceac946e1bf83f
ash <wc47501@gmail.com>
2018-06-13 12:03:11 +0800

    修改对es6继承的理解

--- src/JS-OOP/ES6-Class/Extends.js
+++ src/JS-OOP/ES6-Class/Extends.js
@@ -1,7 +1,10 @@
 /**
  * 使用extends关键字
  * 子类必须在constructor方法中调用super方法，否则新建实例时会报错
- * 本质上是对组合继承做的语法糖，更多语法细节参看阮老师的博客
+ * ES5 的继承（寄生组合继承），实质是先创造子类的实例对象this，然后再将父类的方法添加到this上面（Parent.apply(this)）。
+ * ES6 的继承机制完全不同，实质是先创造父类的实例对象this（所以必须先调用super方法），然后再用子类的构造函数修改this。
+ * babel编译后的polyfill则还是es5继承的实现
+ * 更多语法细节参看阮老师的博客
  * 出处：ECMAScript 6 入门（阮一峰）
  */
 class ColorPoint extends Point {
