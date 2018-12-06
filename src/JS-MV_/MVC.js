96b796804a52469e5df7d4109b5cb43a8c95911e
wc5858 <wc47501@gmail.com>
2018-12-06 22:07:58 +0800

    MV*初探

---
+++ src/JS-MV_/MVC.js
@@ -0,0 +1,13 @@
+// Model：业务数据
+// View：用户界面
+// Controller：管理逻辑和用户输入
+
+// View可以直接驱动Model
+// Controller非常薄，业务逻辑都在View上
+
+// 在GoF看来，MVC实际上是观察者（或订阅/发布模式）、策略和组合模式的变体，也可以再利用上工厂模式和模板模式
+// 在MVC的不同实现中，Controller的作用有强有弱
+
+// 参考资料
+// http://www.ruanyifeng.com/blog/2015/02/mvcmvp_mvvm.html
+// https://www.zhihu.com/question/20148405
