75dc051e16ee484e626a0e21f4902c42c39c422f
wc5858 <wc47501@gmail.com>
2018-12-06 16:56:08 +0800

    mixin

---
+++ src/JS-Design-Patterns/Mixin.js
@@ -0,0 +1,8 @@
+// Mixin模式
+// 将A对象的功能注入到B对象中
+// 来源：https://addyosmani.com/resources/essentialjsdesignpatterns/book/
+
+// 似乎为这本书所特有，我个人不是很看好
+// 可理解性很差，而且不利于改动
+// “不应该只为了复用代码而使用继承”，这里Mixin可以说是继承的一种形式
+// 可以使用组合代替继承：比如说策略模式
